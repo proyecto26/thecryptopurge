a9d993b017b0a4d45fa6383cbc0a6ee9561b5000
Juan David Nicholls Cardona <jdnichollsc@hotmail.com>
2021-10-25 02:02:06 -0500

    Fix router for GitHub deploy

--- app/src/App.js
+++ app/src/App.js
@@ -2,7 +2,7 @@ import {
   Redirect,
   Route,
   Switch,
-  BrowserRouter as Router
+  HashRouter as Router
 } from "react-router-dom";
 import Home from "./pages/Home";
 import Game from "./pages/Game";
@@ -10,7 +10,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router basename="/thecryptopurge">
+    <Router>
       <Switch>
         <Route path="/home" component={Home} exact />
         <Route path="/game" component={Game} exact />
