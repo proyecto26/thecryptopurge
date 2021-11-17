441556e246a8cbcea0b36b7b5236a98d3069c3cf
Juan David Nicholls Cardona <jdnichollsc@hotmail.com>
2021-11-16 21:50:24 -0500

    Code review

--- crypto/scripts/game-script.js
+++ crypto/scripts/game-script.js
@@ -1,15 +1,16 @@
-const { getContractBalance } = require('./utils');
+const { getContractBalance, getAccountBalance } = require('./utils');
 const { deployNFTContract } = require('./nft-contract');
 const { deployGameContract, characters } = require('./game-contract');
 
 const main = async () => {
-  const nftContract = await deployNFTContract();
-  const contract = await deployGameContract(nftContract);
-  const balance = await getContractBalance(contract.address);
-  console.log('Contract balance:', balance);
-
   const [owner] = await hre.ethers.getSigners();
   console.log('Owner address:', owner.address);
+  console.log('Account balance:', await getAccountBalance(owner));
+
+  const nftContract = await deployNFTContract();
+  const contract = await deployGameContract(nftContract);
+  console.log('Contract balance:', await getContractBalance(contract.address));
+  console.log('Account balance:', await getAccountBalance(owner));
 
   const countCharacters = Object.keys(characters).length;
   // Mint NFTs for characters.
