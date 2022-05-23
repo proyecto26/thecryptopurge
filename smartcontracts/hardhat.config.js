97d461ffe64d7b2abaca288de96f6595477836ca
Luis Lucena <lucenaluis137@gmail.com>
2022-05-23 01:55:12 -0400

    VRF update to V2

--- smartcontracts/hardhat.config.js
+++ smartcontracts/hardhat.config.js
@@ -1,85 +1,127 @@
-require('@nomiclabs/hardhat-waffle');
-require('@nomiclabs/hardhat-ethers');
-require("@nomiclabs/hardhat-etherscan");
-require("@nomiclabs/hardhat-web3");
-require('@appliedblockchain/chainlink-plugins-random-number-consumer');
-require("@appliedblockchain/chainlink-plugins-fund-link");
-require('hardhat-deploy');
-require('solidity-coverage');
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-require('./tasks/accounts');
-require('./tasks/balance');
-require('./tasks/block-number');
-require('./tasks/withdraw-link');
-
-require('dotenv').config();
-
-const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL || process.env.ALCHEMY_MAINNET_RPC_URL || 'https://eth-mainnet.alchemyapi.io/v2/your-api-key'
-const RINKEBY_RPC_URL = process.env.ALCHEMY_RINKEBY_RPC_URL || 'https://eth-rinkeby.alchemyapi.io/v2/your-api-key'
-const POLYGON_MAINNET_RPC_URL = process.env.POLYGON_MAINNET_RPC_URL || 'https://polygon-mainnet.alchemyapi.io/v2/your-api-key'
-const MNEMONIC = process.env.MNEMONIC || 'your mnemonic'
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || 'Your etherscan API key'
-// optional
-const PRIVATE_KEY = process.env.PRIVATE_KEY || 'your private key'
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("./tasks")
+require("@appliedblockchain/chainlink-plugins-fund-link")
+require("dotenv").config()
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+const MAINNET_RPC_URL =
+  process.env.MAINNET_RPC_URL ||
+  process.env.ALCHEMY_MAINNET_RPC_URL ||
+  "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
+const RINKEBY_RPC_URL =
+  process.env.RINKEBY_RPC_URL || "https://eth-rinkeby.alchemyapi.io/v2/your-api-key"
+const KOVAN_RPC_URL = process.env.KOVAN_RPC_URL || "https://eth-kovan.alchemyapi.io/v2/your-api-key"
+const POLYGON_MAINNET_RPC_URL =
+  process.env.POLYGON_MAINNET_RPC_URL || "https://polygon-mainnet.alchemyapi.io/v2/your-api-key"
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+// optional
+const MNEMONIC = process.env.MNEMONIC || "Your mnemonic"
+const FORKING_BLOCK_NUMBER = process.env.FORKING_BLOCK_NUMBER
+
+// Your API key for Etherscan, obtain one at https://etherscan.io/
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "Your etherscan API key"
+const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY || "Your polygonscan API key"
+const REPORT_GAS = process.env.REPORT_GAS || false
+
 module.exports = {
-  solidity: {
-    compilers: [
-      {
-        version: "0.4.24"
-      },
-      {
-        version: "0.6.6"
-      },
-      {
-        version: "0.8.4"
-      }
-    ]
-  },
-  //defaultNetwork: 'localhost',
+  defaultNetwork: "hardhat",
   networks: {
+    hardhat: {
+      // If you want to do some forking set `enabled` to true
+      forking: {
+        url: MAINNET_RPC_URL,
+        blockNumber: FORKING_BLOCK_NUMBER,
+        enabled: false,
+      },
+      chainId: 31337,
+    },
+    localhost: {
+      chainId: 31337,
+    },
+    kovan: {
+      url: KOVAN_RPC_URL,
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+      //accounts: {
+      //     mnemonic: MNEMONIC,
+      // },
+      saveDeployments: true,
+      chainId: 42,
+    },
     rinkeby: {
       url: RINKEBY_RPC_URL,
-      accounts: [PRIVATE_KEY],
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+      //   accounts: {
+      //     mnemonic: MNEMONIC,
+      //   },
       saveDeployments: true,
+      chainId: 4,
     },
     mainnet: {
-      chainId: 1,
       url: MAINNET_RPC_URL,
-      accounts: {
-        mnemonic: MNEMONIC,
-      },
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+      //   accounts: {
+      //     mnemonic: MNEMONIC,
+      //   },
       saveDeployments: true,
+      chainId: 1,
     },
     polygon: {
       url: POLYGON_MAINNET_RPC_URL,
-      accounts: [PRIVATE_KEY],
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
       saveDeployments: true,
+      chainId: 137,
     },
   },
   etherscan: {
-    // Your API key for Etherscan
-    // Obtain one at https://etherscan.io/
-    apiKey: ETHERSCAN_API_KEY
+    // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
+    apiKey: {
+      rinkeby: ETHERSCAN_API_KEY,
+      kovan: ETHERSCAN_API_KEY,
+      polygon: POLYGONSCAN_API_KEY,
+    },
+  },
+  gasReporter: {
+    enabled: REPORT_GAS,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+    // coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+  },
+  contractSizer: {
+    runOnCompile: false,
+    only: ["RandomNumberConsumer"],
   },
   namedAccounts: {
     deployer: {
       default: 0, // here this will by default take the first account as deployer
-      1: 0 // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+      1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
     },
     feeCollector: {
-      default: 1
-    }
+      default: 1,
+    },
   },
+    solidity: {
+      compilers: [
+        {
+          version: "0.4.24"
+        },
+        {
+          version: "0.6.6"
+        },
+        {
+          version: "0.8.4"
+        }
+      ]
+    },
   mocha: {
-    timeout: 20000
-  }
-};
+    timeout: 200000, // 200 seconds max for running tests
+  },
+}
